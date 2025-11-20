0498c99e663d6dc2d36a1f50df51ce0ef8a1c0af
nps-alexandralalor <alexandra_lalor@nps.gov>
2025-11-20 09:00:46 -0500

    remove extra code

--- scripts/Field/Field_plot-locations.R
+++ scripts/Field/Field_plot-locations.R
@@ -14,8 +14,6 @@ library(dplyr)
 library(purrr)
 library(here)
 
-# designate target park(s)
-target_park <- "SHEN"
 
 # load in data.
 path_file <- "C:/Users/alalor/OneDrive - DOI/NER/FireFX/FFI Data Management/Exports/"
@@ -43,31 +41,3 @@ for(i in parks) {
   # save file
   write.csv(data_final, paste0(path_output, target_park, "_PlotLocations.csv"), quote=FALSE, row.names = FALSE, na = "") 
 }
-
-  
-
- 
-# # load in data.
-# path_file <- "C:/Users/alalor/OneDrive - DOI/NER/FireFX/FFI Data Management/Exports/"
-# path_data <- paste0(path_file, target_park, "/")
-# path_output <- paste0(here(), "/output/data_clean/")
-# path_output <- "C:/Users/alalor/OneDrive - DOI/R/NER/output/data_clean/"
-# 
-# # load CSV
-# df_raw <- read.csv(paste0(path_data, target_park, "_MetadataReport.csv"), quote = "")
-# 
-# # group
-# df_locations <- df_raw %>% 
-#   select(!c(SampleEventTeam, SampleEventComment, LegacyMonStatus, MonStatus, Protocols, Visited, SampleEventDate)) %>% 
-#   distinct() %>% 
-#   select(c(AdministrationUnit_Name, Purpose, Type, Macroplot, ProjectUnit, Latitude, Longitude, UTM_X, UTM_Y, UTM_Zone, Datum))
-# 
-# # filter
-# df_locations <- df_locations %>% 
-#   filter(!is.na(Latitude),
-#          !is.na(Longitude))
-# 
-# # save file
-# write.csv(df_final, paste0(path_output, target_park, "_PlotLocations.csv"), quote=FALSE, row.names = FALSE, na = "") 
-
-
